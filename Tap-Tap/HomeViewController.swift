4429fb2499e3848df70a2716d5be21bda8e0b11a
Roger Ingouacka <roger.ingouacka@gmail.com>
2017-01-11 21:22:03 +0100

    Ajout d un autre test

--- Tap-Tap/HomeViewController.swift
+++ Tap-Tap/HomeViewController.swift
@@ -18,6 +18,7 @@ class HomeViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Ceci est un Test")
+        print("Ceci est un autre test")
         // Do any additional setup after loading the view.
     }
 
